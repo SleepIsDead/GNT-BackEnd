3b5b14cc5207f46e7eb55d2238a487f062b36392
devjaem <wjdwoals9292@gmail.com>
2023-08-06 23:21:42 +0900

    0806 초기설정 준비

---
+++ src/test/java/travelmakerbackend/TravelMakerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package travelmakerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelMakerBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
