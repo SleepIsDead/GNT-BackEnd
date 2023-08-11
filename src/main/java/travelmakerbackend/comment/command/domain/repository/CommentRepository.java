d6d36041edbccef741fe5d408bd0017e7ca1ad9c
devjaem <wjdwoals9292@gmail.com>
2023-08-11 23:41:33 +0900

    08-11 초기설정 commit2

--- src/main/java/travelmakerbackend/comment/command/domain/repository/CommentRepository.java
+++ src/main/java/travelmakerbackend/comment/command/domain/repository/CommentRepository.java
@@ -1,15 +1,13 @@
 package travelmakerbackend.comment.command.domain.repository;
 
-        import com.ohgiraffers.forepeproject.comment.command.domain.aggregate.entity.CommentEntity;
-        import org.springframework.data.jpa.repository.JpaRepository;
-        import org.springframework.stereotype.Repository;
 
-        import javax.xml.stream.events.Comment;
-        import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import travelmakerbackend.comment.command.domain.aggregate.entity.CommentEntity;
 
 @Repository
 public
 interface CommentRepository extends JpaRepository<CommentEntity, Integer> {
-//        Optional<Object> findById(Long commentId);
+
 }
 
