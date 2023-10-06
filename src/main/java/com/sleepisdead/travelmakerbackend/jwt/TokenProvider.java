636cc8560927ddd0411157f0bb7b626a2a5f042a
jaemin <wjdwoals9292@gmail.com>
2023-10-06 10:15:20 +0900

    [REFACTOR][COMMON] 코드 개선

--- src/main/java/com/sleepisdead/travelmakerbackend/jwt/TokenProvider.java
+++ src/main/java/com/sleepisdead/travelmakerbackend/jwt/TokenProvider.java
@@ -15,8 +15,6 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
 import com.sleepisdead.travelmakerbackend.member.command.application.dto.MemberDTO;
 
